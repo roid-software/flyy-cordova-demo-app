e394f54a5fbaf9b0548c4739ef11f49ca57e2a3a
Pooja Deshpande <pooja.is.deshpande@gmail.com>
2021-10-05 18:21:11 +0530

    Added firebase code for cordova client app

--- www/js/index.js
+++ www/js/index.js
@@ -28,140 +28,189 @@ function onDeviceReady() {
     document.getElementById('deviceready').classList.add('ready');
     
     cordova.plugins.FlyyPlugin.setAppPackage("theflyy.com.flyysdk", function (data) {
-//        alert("Set Package Name: " + data)
+        console.log("Set Package Name: " + data)
     }, function (data) {
         alert("Set Package Name Error: "+data)
     });
     
     cordova.plugins.FlyyPlugin.initSdk("8fce343fa6170bf1ecb5", "stage", function (data) {
-//        alert("Init SDK Success: " + data)
+        console.log("Init SDK Success: " + data)
     }, function (data) {
         alert("Init SDK Error : " + data)
     });
     
     cordova.plugins.FlyyPlugin.setUser("9844313418", "all_users", function (data) {
-//        alert("Set User Success : " + data)
+        console.log("Set User Success : " + data)
     }, function (data) {
         alert("Set User Error : " + data)
     });
     
     cordova.plugins.FlyyPlugin.setUsername("Pooja", function (data) {
-//        alert("Set User Name Success : " + data)
+        console.log("Set User Name Success : " + data)
     }, function (data) {
         alert("Set User Name Error : " + data)
     });
     
     cordova.plugins.FlyyPlugin.startOfferActivity("all_users", function (data) {
-//        alert("Offers Page Success : " + data)
+        console.log("Offers Page Success : " + data)
     }, function (data) {
-        alert("Offers Page Error : " + data)
+        console.log("Offers Page Error : " + data)
     });
     
-//    cordova.plugins.FlyyPlugin.startRewardsActivity("all_users", function (data) {
-//        alert("Rewards Page Success : " + data)
-//    }, function (data) {
-//        alert("Rewards Page Error : " + data)
-//    });
-
-//    cordova.plugins.FlyyPlugin.startWalletActivity("all_users", function (data) {
-//        alert("Wallet Page Success : " + data)
-//    }, function (data) {
-//        alert("Wallet Page Error : " + data)
-//    });
-    
-//    cordova.plugins.FlyyPlugin.trackEvent("testing", "cordova", function (data) {
-//        alert("Track Event Success : " + data)
-//    }, function(data) {
-//        alert("Track Event Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.trackEventJson("testing", "cordova", function (data) {
-//        alert("Track Event Success : " + data)
-//    }, function(data) {
-//        alert("Track Event Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.setRewardGridSpanCount("3", function (data) {
-//        alert("Rewards grid count Success : " + data)
-//    }, function(data) {
-//        alert("Rewards grid count Error : " + data)
-//    });
-
-//    cordova.plugins.FlyyPlugin.handleNotification("userdata", function (data) {
-//        alert("Handle notification Success : " + data)
-//    }, function(data) {
-//        alert("Hanle notification Error : " + data)
-//    });
-//
-    cordova.plugins.FlyyPlugin.setThemeColor("#ffa500", "#ffa500", function (data) {
-//        alert("Set theme color Success : " + data)
-        console.log("Set theme color Success : " + data)
-    }, function(data) {
-        alert("Set theme color Error : " + data)
+    cordova.plugins.FlyyPlugin.startRewardsActivity("all_users", function (data) {
+        console.log("Rewards Page Success : " + data)
+    }, function (data) {
+        console.log("Rewards Page Error : " + data)
     });
     
-    
-    cordova.plugins.FlyyPlugin.onSDKClosedWithScreenName(function (data) {
-        alert("ON FLYY SDK CLOSED : " + data)
-        console.log("ON FLYY SDK CLOSED : ")
+    cordova.plugins.FlyyPlugin.startWalletActivity("all_users", function (data) {
+        console.log("Wallet Page Success : " + data)
+    }, function (data) {
+        console.log("Wallet Page Error : " + data)
     });
     
-
-//    cordova.plugins.FlyyPlugin.getShareData("", function (data) {
-//        alert("getShareData Success : " + data)
-//    }, function(data) {
-//        alert("getShareData Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getReferralCount(function (data) {
-//        alert("getReferralCount Success : " + data)
-//    }, function(data) {
-//        alert("getReferralCount Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getScratchCardCount(function (data) {
-//        alert("getScratchCardCount Success : " + data)
-//    }, function(data) {
-//        alert("getScratchCardCount Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getPreviousLeaderboardWinners("test", function (data) {
-//        alert("getPreviousLeaderboardWinners Success : " + data)
-//    }, function(data) {
-//        alert("getPreviousLeaderboardWinners Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getLeaderboardParticipants("test", function (data) {
-//        alert("getLeaderboardParticipants Success : " + data)
-//    }, function(data) {
-//        alert("getLeaderboardParticipants Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getWalletBalance("points", function (data) {
-//        alert("getWalletBalance Success : " + data)
-//    }, function(data) {
-//        alert("getWalletBalance Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getReferrerDetails(function (data) {
-//        alert("getReferrerDetails Success : " + data)
-//    }, function(data) {
-//        alert("getReferrerDetails Error : " + data)
-//    });
-//
-//    cordova.plugins.FlyyPlugin.getOffersCount(function (data) {
-//        alert("getOffersCount Success : " + data)
-//    }, function(data) {
-//        alert("getOffersCount Error : " + data)
-//    });
+    //    cordova.plugins.FlyyPlugin.trackEvent("testing", "cordova", function (data) {
+    //        alert("Track Event Success : " + data)
+    //    }, function(data) {
+    //        alert("Track Event Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.trackEventJson("testing", "cordova", function (data) {
+    //        alert("Track Event Success : " + data)
+    //    }, function(data) {
+    //        alert("Track Event Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.setRewardGridSpanCount("3", function (data) {
+    //        console.log("Rewards grid count Success : " + data)
+    //    }, function(data) {
+    //        console.log("Rewards grid count Error : " + data)
+    //    });
+    
+    //    cordova.plugins.FlyyPlugin.handleNotification("userdata", function (data) {
+    //        alert("Handle notification Success : " + data)
+    //    }, function(data) {
+    //        alert("Hanle notification Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.handleForeGroundNotification("userdata", function (data) {
+    //        alert("Handle notification Success : " + data)
+    //    }, function(data) {
+    //        alert("Hanle notification Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.handleBackGroundNotification("userdata", function (data) {
+    //        alert("Handle notification Success : " + data)
+    //    }, function(data) {
+    //        alert("Hanle notification Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.setThemeColor("#ffa500", "#ffa500", function (data) {
+    //        console.log("Set theme color Success : " + data)
+    //    }, function(data) {
+    //        alert("Set theme color Error : " + data)
+    //    });
     
     
-//    cordova.plugins.FlyyPlugin.setNewUser("9844313418", function (data) {
-//        alert("Set New User Success : " + data)
-//    }, function (data) {
-//        alert("Set New User Error : " + data)
-//    });
+    //    cordova.plugins.FlyyPlugin.onSDKClosedWithScreenName(function (data) {
+    //        console.log("ON FLYY SDK CLOSED : ")
+    //    });
     
     
+    //    cordova.plugins.FlyyPlugin.getShareData("", function (data) {
+    //        alert("getShareData Success : " + data)
+    //    }, function(data) {
+    //        alert("getShareData Error : " + data)
+    //    });
+    ////
+    //    cordova.plugins.FlyyPlugin.getReferralCount(function (data) {
+    //        alert("getReferralCount Success : " + data)
+    //    }, function(data) {
+    //        alert("getReferralCount Error : " + data)
+    //    });
+    ////
+    //    cordova.plugins.FlyyPlugin.getScratchCardCount(function (data) {
+    //        alert("getScratchCardCount Success : " + data)
+    //    }, function(data) {
+    //        alert("getScratchCardCount Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.getPreviousLeaderboardWinners("test", function (data) {
+    //        alert("getPreviousLeaderboardWinners Success : " + data)
+    //    }, function(data) {
+    //        alert("getPreviousLeaderboardWinners Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.getLeaderboardParticipants("test", function (data) {
+    //        alert("getLeaderboardParticipants Success : " + data)
+    //    }, function(data) {
+    //        alert("getLeaderboardParticipants Error : " + data)
+    //    });
+    //
+    //    cordova.plugins.FlyyPlugin.getWalletBalance("points", function (data) {
+    //        alert("getWalletBalance Success : " + data)
+    //    }, function(data) {
+    //        alert("getWalletBalance Error : " + data)
+    //    });
+    ////
+    //    cordova.plugins.FlyyPlugin.getReferrerDetails(function (data) {
+    //        alert("getReferrerDetails Success : " + data)
+    //    }, function(data) {
+    //        alert("getReferrerDetails Error : " + data)
+    //    });
+    ////
+    //    cordova.plugins.FlyyPlugin.getOffersCount(function (data) {
+    //        alert("getOffersCount Success : " + data)
+    //    }, function(data) {
+    //        alert("getOffersCount Error : " + data)
+    //    });
+    //
+    //
+    //    cordova.plugins.FlyyPlugin.setNewUser("9844313418", function (data) {
+    //        console.log("Set New User Success : " + data)
+    //    }, function (data) {
+    //        alert("Set New User Error : " + data)
+    //    });
+    //
+    //
+    //
     
+    
+    //Firebase messaging code
+    
+    cordova.plugins.firebase.messaging.requestPermission().then(function() {
+        console.log("Push messaging is allowed");
+    });
+    
+    cordova.plugins.firebase.messaging.getToken().then(function(token) {
+        console.log("Got device token: ", token);
+        cordova.plugins.FlyyPlugin.sendFCMTokenToServer(token, function (data) {
+            alert("FCM Token Success : " + data)
+        });
+    });
+    
+    cordova.plugins.firebase.messaging.onTokenRefresh(function() {
+        console.log("Device token updated");
+        cordova.plugins.FlyyPlugin.sendFCMTokenToServer(token, function (data) {
+            alert("FCM Token Success : " + data)
+        });
+    });
+    
+    cordova.plugins.firebase.messaging.onBackgroundMessage(function(payload) {
+        console.log("New background FCM message: ", payload);
+        cordova.plugins.FlyyPlugin.handleBackGroundNotification("userdata", function (data) {
+            alert("Handle notification Success : " + data)
+        }, function(data) {
+            alert("Hanle notification Error : " + data)
+        });
+    });
+    
+    cordova.plugins.firebase.messaging.onMessage(function(payload) {
+        console.log("New foreground FCM message: ", payload);
+        cordova.plugins.FlyyPlugin.handleForeGroundNotification("userdata", function (data) {
+            alert("Handle notification Success : " + data)
+        }, function(data) {
+            alert("Hanle notification Error : " + data)
+        });
+    });
 }
